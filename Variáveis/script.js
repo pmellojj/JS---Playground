2115694d1b79a83c92f2ab7c3194af2a38826aa8
Pedro Mello <145684198+pmellojj@users.noreply.github.com>
2023-09-27 16:51:21 +0100

    Exercício Variáveis de Cada Tipo

---
+++ Variáveis/script.js
@@ -0,0 +1,10 @@
+//Var String
+var nomeCompleto = 'Pedro Gonçalves Mello';
+//Var Number
+var idade = 24;
+//Var Boolean
+var militar = true;
+
+console.log("String:", nomeCompleto);
+console.log("Number:", idade);
+console.log("Boolean:", militar);
